f81d6f31f8289135c1e47d994fa651462ac658d9
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-07-04 02:48:50 +0900

    update GNB3

---
+++ js/gnb3.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.gnb li').on('mouseover focusin',function(){
+        $(this).children('ul').stop().fadeIn(300);
+        $('.bg_gnb').stop().fadeIn(300);
+    });
+    $('.gnb li').on('mouseleave',function(){
+        $(this).children('ul').stop().fadeOut(200);
+        $('.bg_gnb').stop().fadeOut(200);
+    });
+    $(".gnb").on("mouseover focusin",function(){
+        $('.bg_gnb').stop().fadeIn(300);
+    });
+    $('gnb > li').on('focusout',function(){
+        $(this).children('ul').stop().fadeOut(200);
+    })
+})
